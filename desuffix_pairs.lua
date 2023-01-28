739d5fb43ba46c0f4092a58da32aa85c767c8580
Eike Decker <zet23t@googlemail.com>
2023-01-28 11:09:46 +0100

    desuffix pairs (a horrible method) allows now numerical gaps

--- desuffix_pairs.lua
+++ desuffix_pairs.lua
@@ -2,17 +2,22 @@
 -- the 2nd value as index. E.g. foo_1 is returned as index_1 element with "foo" as name
 local function desuffixed_pairs(t)
 	local values = {}
+	local max_id
 	for k, v in pairs(t) do
 		local name, id, icon = k:match "([^_]*)_?([^_]+)_?([^_]*)"
 		id = tonumber(id)
 		icon = tonumber(icon)
 		-- require "log"("%s %s %s %s",tostring(id),tostring(name),tostring(id), tostring(icon))
 		assert(id)
+		max_id = math.max(id, max_id or id)
 		values[(id)] = { name, v, icon }
 	end
 	local i = 0
+	
 	return function()
-		i = i + 1
+		repeat
+			i = i + 1
+		until values[i] or i > max_id
 		if not values[i] then return end
 		return unpack(values[i])
 	end
