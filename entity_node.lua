0e0123e96108f9a36435e0431f8e8d5fa7fe48d6
Eike Decker <zet23t@googlemail.com>
2023-03-04 22:22:40 +0100

    Entity node component system
    Similar to the ui_rect but more generic

---
+++ entity_node.lua
@@ -0,0 +1,138 @@
+local assert_type = require "love-util.assert_type"
+local class       = require "love-util.class"
+
+---@class entity_component : object
+---@field entity entity_node
+
+---@class entity_node : object An entity represents an element in a tree structure
+---@field parent entity_node|nil
+---@field children entity_node[]
+---@field components table[]
+---@field component_data table
+---@field is_enabled boolean
+---@field name string
+local entity_node = class "entity_node"
+entity_node.is_enabled = true
+entity_node.name = "unnamed entity_node"
+
+local function call_on(t, name, ...)
+	if type(t[name]) ~= "function" then return false, t[name] end
+	return true, t[name](t, ...)
+end
+
+function entity_node:new(name)
+	return self:create {
+		name = assert_type(name, "string", "nil"),
+		component_data = {},
+		children = {},
+		components = {},
+	}
+end
+
+function entity_node:set_parent(parent)
+	parent:add_child(self)
+	return self
+end
+
+function entity_node:add_child(node)
+	node:remove()
+	if self:has_parent(node) then
+		print("Warning: rejected element in add_child")
+		return self
+	end
+	node.parent = self
+	self.children[#self.children + 1] = node
+	return self
+end
+
+function entity_node:has_parent(parent)
+	if self.parent == parent then return true end
+	return self.parent and self.parent:has_parent(parent)
+end
+
+function entity_node:remove()
+	local parent = self.parent
+	if not parent then return end
+	self.parent = nil
+	for i = #parent.children, 1, -1 do
+		if parent.children[i] == self then
+			table.remove(parent.children, i)
+			break
+		end
+	end
+	return self
+end
+
+function entity_node:call_on_components(name, ...)
+	for i = #self.components, 1, -1 do
+		call_on(self.components[i], name, ...)
+	end
+	return self
+end
+
+---@param steps table
+---@param ... any
+function entity_node:call(steps, ...)
+	for i = 1, #steps do
+		local step = steps[i]
+		local mode = step.mode
+		if mode == "ignore_disabled" then
+			if not self.is_enabled then
+				return
+			end
+		elseif mode == "select" then
+			local name = step.name
+			for i = 1, #self.components do
+				local cmp = self.components[i]
+				local selected = false
+				if cmp[name] then
+					if cmp[name](cmp, self, ...) then
+						selected = true
+						break
+					else
+						return
+					end
+				end
+				if not selected then 
+					return 
+				end
+			end
+		elseif mode == "components" then
+			local name = step.name
+			for i = #self.components, 1, -1 do
+				call_on(self.components[i], name, ...)
+			end
+		elseif mode == "children" then
+			for i = #self.children, 1, -1 do
+				self.children[i]:call(steps, ...)
+			end
+		end
+	end
+end
+
+function entity_node:set_enabled(enabled)
+	if self.is_enabled == enabled then return self end
+	self.is_enabled = enabled
+	self:call_on_components(enabled and "on_entity_node_enabled" or "on_entity_node_disabled")
+	return self
+end
+
+function entity_node:add_component(cmp)
+	self.components[#self.components + 1] = cmp
+	cmp.entity = self
+	self:call_on_components("on_component_added", cmp)
+	return self
+end
+
+function entity_node:remove_component(cmp)
+	for i = 1, #self.components do
+		if self.components[i] == cmp then
+			table.remove(self.components, i)
+			self:call_on_components("on_component_removed", cmp)
+			break
+		end
+	end
+	return self
+end
+
+return entity_node
