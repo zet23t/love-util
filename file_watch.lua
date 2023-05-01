e8018a3b2e9154f6eef8150a2f77923ed1443eba
Eike Decker <zet23t@googlemail.com>
2023-05-01 22:52:27 +0200

    Simple file watcher util method
    allows adding custom quick reloads for assets

---
+++ file_watch.lua
@@ -0,0 +1,42 @@
+local nativefs = require "nativefs"
+
+local file_watch = {
+	watches = {},
+}
+
+local function check_watch(self, watch)
+	local path = watch.path
+	local info = nativefs.getInfo(path)
+	if info and info.type == "file" and info.modtime ~= watch.modtime then
+		watch.modtime = info.modtime
+		watch.callback(path)
+	end
+end
+
+function file_watch:add(path, callback)
+	local watch = {
+		path = path,
+		callback = callback
+	}
+	self.watches[#self.watches+1] = watch
+	check_watch(self, watch)
+	return self
+end
+
+function file_watch:remove(path)
+	for i=#self.watches,1,-1 do
+		if self.watches[i].path == path then
+			table.remove(self,i)
+		end
+	end
+	return self
+end
+
+function file_watch:check()
+	for i=1,#self.watches do
+		local watch = self.watches[i]
+		check_watch(self, watch)
+	end
+end
+
+return file_watch
